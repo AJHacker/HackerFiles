c34781e1b6e7afaed83997f95154c7ca6c9d091e
Arihant Jain <Arihant@new-host.home>
2013-02-26 17:40:26 -0500

    Initial Commit

---
+++ hacker/hackerView.h
@@ -0,0 +1,13 @@
+//
+//  hackerView.h
+//  hacker
+//
+//  Created by Arihant Jain on 2/26/13.
+//  Copyright (c) 2013 AJcoder. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface hackerView : ScreenSaverView
+
+@end
---
+++ hacker/hackerView.m
@@ -0,0 +1,52 @@
+//
+//  hackerView.m
+//  hacker
+//
+//  Created by Arihant Jain on 2/26/13.
+//  Copyright (c) 2013 AJcoder. All rights reserved.
+//
+
+#import "hackerView.h"
+
+@implementation hackerView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
